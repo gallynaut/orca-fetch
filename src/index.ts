d55fd09f48cb3fe06c182b8fa4c9d9120b1f8042
gallynaut <Connerngallagher@gmail.com>
2021-10-03 14:29:06 -0600

    working

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Connection } from "@solana/web3.js";
-import { getOrca, OrcaPoolConfig, OrcaU64 } from "@orca-so/sdk";
+import { Orca, getOrca, OrcaPool, OrcaPoolConfig, OrcaU64 } from "@orca-so/sdk";
+import Decimal from "decimal.js";
 
 async function main() {
   // let connection = new Connection("https://api.mainnet-beta.solana.com");
@@ -15,27 +16,32 @@ async function main() {
       "singleGossip"
     );
     const orca = getOrca(connection);
-
-    // Get an instance of the ETH-USDC orca pool
-    const pool = orca.getPool(OrcaPoolConfig.USDC_USDT);
-    const supply = await pool.getLPSupply();
-    console.log(supply.toNumber());
-
-    console.log(await pool.getTokenA());
-    console.log(await pool.getTokenB());
+    const q = await fetchOrcaLPQuote(orca, OrcaPoolConfig.USDC_USDT);
   } catch (e) {
     // catch
   }
 }
+async function fetchOrcaLPQuote(
+  orca: Orca,
+  poolConfig: OrcaPoolConfig
+): Promise<number> {
+  const pool = orca.getPool(poolConfig);
+  const one = new Decimal(1);
+  const quote = await pool.getQuote(pool.getTokenA(), one, one);
+  console.log(quote);
+  console.log(poolConfig, quote.getRate());
+  console.log(quote.getExpectedOutputAmount().toNumber());
+  return 0;
+}
 
-// main().then(
-//   () => {
-//     // logger.info("Ran node successfully.");
-//     // process.exit();
-//   },
-//   (err) => {
-//     // logger.error("Node failure.");
-//     // logger.error(err);
-//     // process.exit(-1);
-//   }
-// );
+main().then(
+  () => {
+    // logger.info("Ran node successfully.");
+    // process.exit();
+  },
+  (err) => {
+    // logger.error("Node failure.");
+    // logger.error(err);
+    // process.exit(-1);
+  }
+);
