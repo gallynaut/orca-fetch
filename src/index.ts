928b6cb3c48da01eedcabddb6af2ee0636769ee6
gallynaut <Connerngallagher@gmail.com>
2021-10-04 11:04:11 -0600

    working

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import {
   OrcaPoolConfig,
   getTokenCount,
   PoolTokenCount,
-  U64Utils,
   OrcaU64,
 } from "@orca-so/sdk";
 import { orcaPoolConfigs } from "@orca-so/sdk/dist/constants/pools";
+import Decimal from "decimal.js";
 
 async function main() {
   try {
@@ -21,22 +21,47 @@ async function main() {
     // get public key of pool
     const pk = new PublicKey(OrcaPoolConfig.SOL_USDC);
     console.log(pk.toString());
-
     const pool = orca.getPool(OrcaPoolConfig.SOL_USDC);
+    const supply = (await pool.getLPSupply()).toNumber();
     const poolParams = orcaPoolConfigs[pk.toString()];
     const tokenA = pool.getTokenA();
     const tokenB = pool.getTokenB();
 
+    // get current amount of each token in the pool
     const tokenCount: PoolTokenCount = await getTokenCount(
       connection,
       poolParams,
       tokenA,
       tokenB
     );
-    const tokenAAmount = new OrcaU64(tokenCount.inputTokenCount, tokenA.scale);
-    const tokenBAmount = new OrcaU64(tokenCount.outputTokenCount, tokenB.scale);
-    console.log(tokenAAmount.toNumber());
-    console.log(tokenBAmount.toNumber());
+    const tokenAAmount = new OrcaU64(
+      tokenCount.inputTokenCount,
+      tokenA.scale
+    ).toNumber();
+    const tokenBAmount = new OrcaU64(
+      tokenCount.outputTokenCount,
+      tokenB.scale
+    ).toNumber();
+
+    // get current rate of pool tokenA/tokenB
+    const rate = (
+      await pool.getQuote(
+        pool.getTokenA(),
+        new Decimal(1),
+        new Decimal(0.5) // low slippage
+      )
+    )
+      .getRate()
+      .toNumber();
+
+    // calculate price
+    const price = (tokenAAmount * rate + tokenBAmount) / supply;
+    console.log(`$${price} ${tokenA.tag}/${tokenB.tag}`);
+
+    // check if tokenB is USDC
+    if (tokenB.tag !== "USDC") {
+      // find tokenB/USDC
+    }
   } catch (e) {
     // catch
   }
